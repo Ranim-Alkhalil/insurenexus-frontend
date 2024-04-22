55fc2f2f5adbe7512e08917f4c21683c2a23d561
Ramanidalalhaniny <150138424+Ramanidalalhaniny@users.noreply.github.com>
2024-04-22 18:25:46 +0300

    comment

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,9 @@
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { CssBaseline } from "@mui/material";
-import TopAppBar from "./components/base/TopAppBar";
-import LeftDrawer from "./components/base/LeftDrawer";
 import BaseComponent from "./components/base/BaseComponent";
-import Header from "./components/companys select/Header_company";
-import About from "./components/about_us/About";
+import Header from "./components/companys select/companies_page";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CssBaseline />
