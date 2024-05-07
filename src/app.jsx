f1346bb0655c1d1d392a57519f40730c0f8aa519
Ramanidalalhaniny <150138424+Ramanidalalhaniny@users.noreply.github.com>
2024-05-07 21:36:28 +0300

    adding bransh

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { CssBaseline } from "@mui/material";
 import BaseComponent from "./components/base/BaseComponent";
-import Header from "./components/companys select/companies_page";
+import Home_page from "./components/home_page_/Home_page";
+import Footer_pages from "./components/footer_section/Footer_pages";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.render(
   <React.StrictMode>
     <CssBaseline />
     <BaseComponent />
-    <Header />
-  </React.StrictMode>
+    <Home_page />
+    <Footer_pages />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
